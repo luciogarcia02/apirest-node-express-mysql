5e2e13fd0f19004d25ccf4f65cb545bbcdcb8b84
luciogarcia02 <luciogarcialam@gmail.com>
2023-02-22 11:49:54 -0300

    uso de citasRouter

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usuariosRouter = require('./routes/usuarios');
+var citasRouter = require('./routes/citas');
+
 
 var app = express();
 
@@ -21,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/usuarios', usuariosRouter);
+app.use('/citas', citasRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
