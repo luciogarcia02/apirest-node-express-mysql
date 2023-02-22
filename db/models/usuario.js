a7d7499c8f3ae386d595a46ce8838161ace9790b
luciogarcia02 <luciogarcialam@gmail.com>
2023-02-22 11:50:10 -0300

    agrego asociacion con citas

--- db/models/usuario.js
+++ db/models/usuario.js
@@ -11,6 +11,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Usuario.hasMany (models.Cita, {
+        foreignKey: 'UsuarioId',
+      })
     }
   }
   Usuario.init({
