8985b508da2d8758e7bc58fe19046063e519562b
Ramiro Garcia Alonso <ramirog154@alu.bue.edu.ar>
2023-02-22 14:28:10 -0300

    post create

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,9 +2,25 @@ var express = require('express');
 var router = express.Router();
 var users = require("../src/repositories/usuarios");
 /* GET users listing. */
-router.get('/hola', async function(req, res) {
+router.get('/', async function(req, res) {
   let dev = await users.getUsers()
   res.json(dev);
 });
 
+
+/*POST create user*/
+router.post('/create',async function(req,res){
+  let usuarioCreate = {}
+  usuarioCreate.firstName="Fabian";
+  usuarioCreate.lastName="Alonso";  
+  usuarioCreate.email="arqalon@gmail.com";
+
+  await users.createUser(usuarioCreate);
+
+    res.status(200);
+ 
+});
+
+
+/*DELETE delete user*/
 module.exports = router;
--- src/repositories/usuarios.js
+++ src/repositories/usuarios.js
@@ -4,6 +4,16 @@ async function getUsers(){
     return await Usuario.findAll();
 }
 
+async function createUser(usuarioCreate){
+    try{
+        Usuario.create(usuarioCreate);
+    }catch(e){
+        console.log(e);
+    }
+        return true;
+}
+
 module.exports = {
     getUsers,
+    createUser,
 }
